85524e947ab94c201ce9c631ad91012e366ec58b
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-03-15 10:02:00 -0400

    ok let's try this

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,49 @@
-#include <Geode/Geode.hpp>
-
-using namespace geode::prelude;
-
-
+#include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/CustomSongWidget.hpp>
+
+bool alreadyCalled = false;
+bool isSongPreviews = false;
+
 class $modify(MyCustomSongWidget, CustomSongWidget) {
+	struct Fields {
+		CCMenuItemSpriteExtra* m_copySongIDButton;
+	};
+	bool init(SongInfoObject* songInfo, CustomSongDelegate* songDelegate, bool showSongSelect, bool showPlayMusic, bool showDownload, bool isRobtopSong, bool unkBool, bool isMusicLibrary, int unk) {
+		if (!CustomSongWidget::init(songInfo, songDelegate, showSongSelect, showPlayMusic, showDownload, isRobtopSong, unkBool, isMusicLibrary, unk)) return false;
 
-    struct Fields {
-        CCMenu* m_copySongIDMenu;
-        CCMenuItemSpriteExtra* m_copySongIDButton;
-    };
-    
-    bool init(SongInfoObject* songInfo, CustomSongDelegate* songDelegate, bool showSongSelect, bool showPlayMusic, bool showDownload, bool isRobtopSong, bool unkBool, bool isMusicLibrary, int unk) {
-        if (!CustomSongWidget::init(songInfo, songDelegate, showSongSelect, showPlayMusic, showDownload, isRobtopSong, unkBool, isMusicLibrary, unk)) return false;
+		if (!m_infoBtn || !m_downloadBtn) return true;
 
-        m_fields->m_copySongIDMenu = CCMenu::create();
-        auto copySongIDButtonSprite = CCSprite::createWithSpriteFrameName("GJ_copyBtn_001.png");
+		const auto fields = m_fields.self();
 
-        if (isMusicLibrary) {
-            this->addChildAtPosition(m_fields->m_copySongIDMenu, Anchor::BottomLeft, ccp(-142.f, -10.f));
-            m_songIDLabel->setPosition(ccp(-131.f, -10.f));
-            copySongIDButtonSprite->setScale(0.375f);
-        } else {
-            this->addChildAtPosition(m_fields->m_copySongIDMenu, Anchor::BottomLeft, ccp(-131.f, -27.f));
-            m_songIDLabel->setPosition(ccp(-115.f, -27.f));
-            copySongIDButtonSprite->setScale(0.5f);
-        }
+		CCSprite* copySongIDButtonSprite = CCSprite::createWithSpriteFrameName("GJ_copyBtn_001.png");
+		copySongIDButtonSprite->setScale(isMusicLibrary ? 0.375f : 0.5f);
+		
+		fields->m_copySongIDButton = CCMenuItemSpriteExtra::create(copySongIDButtonSprite, this, menu_selector(MyCustomSongWidget::onCopySongID));
+		fields->m_copySongIDButton->setPosition({m_downloadBtn->getPositionX() - 31.f, m_downloadBtn->getPositionY() - 5.f});
+		if (isMusicLibrary) {
+			fields->m_copySongIDButton->setPositionY(fields->m_copySongIDButton->getPositionY() - 2.5f);
+			const bool songIsDownloaded = std::filesystem::exists(MusicDownloadManager::sharedState()->pathForSong(m_customSongID));
+			if ((!songIsDownloaded || LevelEditorLayer::get()) && isSongPreviews) fields->m_copySongIDButton->setPositionX(fields->m_copySongIDButton->getPositionX() - 37.5f);
+		}
+		fields->m_copySongIDButton->setID("copy-song-id-button"_spr);
 
-        m_fields->m_copySongIDButton = CCMenuItemSpriteExtra::create(copySongIDButtonSprite, this, menu_selector(MyCustomSongWidget::onCopySongID));
-        m_fields->m_copySongIDMenu->addChildAtPosition(m_fields->m_copySongIDButton, Anchor::Center);
+		m_buttonMenu->addChild(fields->m_copySongIDButton);
 
-        m_fields->m_copySongIDButton->setID("copy-song-id-button");
-        m_fields->m_copySongIDMenu->setID("copy-song-id-menu");
+		return true;
+	}
 
+	void onCopySongID(CCObject* sender) {
+		(void) clipboard::write(fmt::format("{}", m_customSongID));
+		Notification::create(fmt::format("Song ID {} was copied.", m_customSongID))->show();
+	}
+};
 
-        return true;
-    }
-
-    void positionInfoObjects() {
-        if (m_isMusicLibrary) {
-            m_fields->m_copySongIDMenu->setPosition(ccp(-142.f, -10.f));
-            m_songIDLabel->setPosition(ccp(-131.f, -10.f));
-        } else {
-            m_fields->m_copySongIDMenu->setPosition(ccp(-131.f, -27.f));
-            m_songIDLabel->setPosition(ccp(-115.f, -27.f));
-        }
-    }
-
-    void onCopySongID(CCObject* sender) {
-        bool hasSucceeded = clipboard::write(fmt::format("{}", m_customSongID));
-        if (hasSucceeded) {
-            Notification::create("Successfully copied song ID!", NotificationIcon::Success, 1.f)->show();
-        } else {
-            Notification::create("Failed to copy song ID.", NotificationIcon::Error, 1.f)->show();
-        }
-    }
+class $modify(DoesSongPreviewsExist, MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init()) return false;
+		if (alreadyCalled) return true;
+		isSongPreviews = Loader::get()->isModLoaded("spaghettdev.songpreview");
+		alreadyCalled = true;
+		return true;
+	}
 };

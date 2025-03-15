b207bb2eebf63b9eba37869d185aa8290ff25381
JohnnyCena123 <iamahuman1395@gmail.com>
2025-03-15 11:17:26 +0200

    a couple more changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,46 @@ using namespace geode::prelude;
 
 #include <Geode/modify/CustomSongWidget.hpp>
 class $modify(MyCustomSongWidget, CustomSongWidget) {
+
+    struct Fields {
+        CCMenu* m_copySongIDMenu;
+        CCMenuItemSpriteExtra* m_copySongIDButton;
+    };
     
     bool init(SongInfoObject* songInfo, CustomSongDelegate* songDelegate, bool showSongSelect, bool showPlayMusic, bool showDownload, bool isRobtopSong, bool unkBool, bool isMusicLibrary, int unk) {
         if (!CustomSongWidget::init(songInfo, songDelegate, showSongSelect, showPlayMusic, showDownload, isRobtopSong, unkBool, isMusicLibrary, unk)) return false;
 
-        auto copySongIDMenu = CCMenu::create();
-        auto copyButtonSprite = CCSprite::createWithSpriteFrameName("GJ_copyBtn_001.png");
+        m_fields->m_copySongIDMenu = CCMenu::create();
+        auto m_copySongIDButtonSprite = CCSprite::createWithSpriteFrameName("GJ_copyBtn_001.png");
 
-        if (m_isMusicLibrary) {
-            this->addChildAtPosition(copySongIDMenu, Anchor::BottomLeft, ccp(-142.f, -10.f));
+        if (isMusicLibrary) {
+            this->addChildAtPosition(m_fields->m_copySongIDMenu, Anchor::BottomLeft, ccp(-142.f, -10.f));
             m_songIDLabel->setPosition(ccp(-131.f, -10.f));
-            copyButtonSprite->setScale(0.375f);
+            m_copySongIDButtonSprite->setScale(0.375f);
         } else {
-            this->addChildAtPosition(copySongIDMenu, Anchor::BottomLeft, ccp(-131.f, -27.f));
+            this->addChildAtPosition(m_fields->m_copySongIDMenu, Anchor::BottomLeft, ccp(-131.f, -27.f));
             m_songIDLabel->setPosition(ccp(-115.f, -27.f));
-            copyButtonSprite->setScale(0.5f);
+            m_copySongIDButtonSprite->setScale(0.5f);
         }
 
-        auto copyButton = CCMenuItemSpriteExtra::create(copyButtonSprite, this, menu_selector(MyCustomSongWidget::onCopySongID));
-        copySongIDMenu->addChildAtPosition(copyButton, Anchor::Center);
+        m_fields->m_copySongIDButton = CCMenuItemSpriteExtra::create(m_copySongIDButtonSprite, this, menu_selector(MyCustomSongWidget::onCopySongID));
+        m_fields->m_copySongIDMenu->addChildAtPosition(copyButton, Anchor::Center);
 
+        m_fields->m_copySongIDButton->setID("copy-song-id-button");
         copySongIDMenu->setID("copy-song-id-menu");
-        copyButton->setID("copy-song-id-button");
 
 
         return true;
+    }
+
+    void positionInfoObjects() {
+        if (isMusicLibrary) {
+            m_fields->m_copySongIDMenu->setPosition(ccp(-142.f, -10.f));
+            m_songIDLabel->setPosition(ccp(-131.f, -10.f));
+        } else {
+            m_fields->m_copySongIDMenu->setPosition(ccp(-131.f, -27.f));
+            m_songIDLabel->setPosition(ccp(-115.f, -27.f));
+        }
     }
 
     void onCopySongID(CCObject* sender) {

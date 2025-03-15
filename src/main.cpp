f04d938938903dd3e70e06e7cf278cd6620163ef
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-03-15 10:10:33 -0400

    remove fields access :)

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,24 @@ bool alreadyCalled = false;
 bool isSongPreviews = false;
 
 class $modify(MyCustomSongWidget, CustomSongWidget) {
-	struct Fields {
-		CCMenuItemSpriteExtra* m_copySongIDButton;
-	};
 	bool init(SongInfoObject* songInfo, CustomSongDelegate* songDelegate, bool showSongSelect, bool showPlayMusic, bool showDownload, bool isRobtopSong, bool unkBool, bool isMusicLibrary, int unk) {
 		if (!CustomSongWidget::init(songInfo, songDelegate, showSongSelect, showPlayMusic, showDownload, isRobtopSong, unkBool, isMusicLibrary, unk)) return false;
 
-		if (!m_infoBtn || !m_downloadBtn) return true;
-
-		const auto fields = m_fields.self();
+		if (!m_downloadBtn) return true;
 
 		CCSprite* copySongIDButtonSprite = CCSprite::createWithSpriteFrameName("GJ_copyBtn_001.png");
 		copySongIDButtonSprite->setScale(isMusicLibrary ? 0.375f : 0.5f);
 
-		fields->m_copySongIDButton = CCMenuItemSpriteExtra::create(copySongIDButtonSprite, this, menu_selector(MyCustomSongWidget::onCopySongID));
-		fields->m_copySongIDButton->setPosition({m_downloadBtn->getPositionX() - 31.f, m_downloadBtn->getPositionY() - 5.f});
+		copySongIDButton = CCMenuItemSpriteExtra::create(copySongIDButtonSprite, this, menu_selector(MyCustomSongWidget::onCopySongID));
+		copySongIDButton->setPosition({m_downloadBtn->getPositionX() - 31.f, m_downloadBtn->getPositionY() - 5.f});
 		if (isMusicLibrary) {
-			fields->m_copySongIDButton->setPositionY(fields->m_copySongIDButton->getPositionY() - 2.5f);
+			copySongIDButton->setPositionY(copySongIDButton->getPositionY() - 2.5f);
 			const bool songIsDownloaded = std::filesystem::exists(static_cast<std::string>(MusicDownloadManager::sharedState()->pathForSong(m_customSongID)));
-			if ((!songIsDownloaded || LevelEditorLayer::get()) && isSongPreviews) fields->m_copySongIDButton->setPositionX(fields->m_copySongIDButton->getPositionX() - 37.5f);
+			if ((!songIsDownloaded || LevelEditorLayer::get()) && isSongPreviews) copySongIDButton->setPositionX(copySongIDButton->getPositionX() - 37.5f);
 		}
-		fields->m_copySongIDButton->setID("copy-song-id-button"_spr);
+		copySongIDButton->setID("copy-song-id-button"_spr);
 
-		m_buttonMenu->addChild(fields->m_copySongIDButton);
+		m_buttonMenu->addChild(copySongIDButton);
 
 		return true;
 	}

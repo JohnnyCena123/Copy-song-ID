e048f7f69f18caf5521b44df0174394e7d3cfffa
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-03-20 08:57:06 -0400

    one last edge case :P

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ class $modify(MyCustomSongWidget, CustomSongWidget) {
 	bool init(SongInfoObject* songInfo, CustomSongDelegate* songDelegate, bool showSongSelect, bool showPlayMusic, bool showDownload, bool isRobtopSong, bool unkBool, bool isMusicLibrary, int unk) {
 		if (!CustomSongWidget::init(songInfo, songDelegate, showSongSelect, showPlayMusic, showDownload, isRobtopSong, unkBool, isMusicLibrary, unk)) return false;
 
-		if (!m_downloadBtn || m_customSongID < 1) return true;
+		if (!m_downloadBtn || m_customSongID < 1 || m_isRobtopSong) return true;
 
 		CCSprite* copySongIDButtonSprite = CCSprite::createWithSpriteFrameName("GJ_copyBtn_001.png");
 		copySongIDButtonSprite->setScale(isMusicLibrary ? 0.375f : 0.5f);

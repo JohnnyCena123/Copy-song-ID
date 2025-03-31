093a10d0b2dcbac42a51699ebb80c94a64208aca
JohnnyCena123 <iamahuman1395@gmail.com>
2025-03-31 12:36:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 
 using namespace geode::prelude;
 
+bool isSongPreviews;
+bool alreadyCalled;
+ #include <Geode/modify/MenuLayer.hpp>
+ class $modify(DoesSongPreviewsExist, MenuLayer) {
+ 	bool init() {
+ 		if (!MenuLayer::init()) return false;
+ 		if (alreadyCalled) return true;
+ 		isSongPreviews = Loader::get()->isModLoaded("spaghettdev.songpreview");
+ 		alreadyCalled = true;
+ 		return true;
+ 	}
+ };
 
 #include <Geode/modify/CustomSongWidget.hpp>
 class $modify(MyCustomSongWidget, CustomSongWidget) {
@@ -16,7 +28,7 @@ class $modify(MyCustomSongWidget, CustomSongWidget) {
 		CCMenuItemSpriteExtra* copyBtn = CCMenuItemSpriteExtra::create(copyBtnSpr, this, menu_selector(MyCustomSongWidget::onCopySongID));
 		copyBtn->setPosition({m_downloadBtn->getPositionX() - 31.f, m_downloadBtn->getPositionY() - 5.f});
 		if (isMusicLibrary) {
-			copyBtn->setPosition(ccp(m_playbackBtn->getPositionX() + 60.f, m_playbackBtn->getPositionY() + 22.5f));
+			copyBtn->setPosition(ccp(m_playbackBtn->getPositionX() + (isSongPreviews ? 60.f : 20.f), m_playbackBtn->getPositionY() + 22.5f));
 		} else if (showSongSelect) copyBtn->setPosition({m_selectSongBtn->getPositionX() + 19.f, m_selectSongBtn->getPositionY() + 60.f});
 		copyBtn->setID("button"_spr);
 

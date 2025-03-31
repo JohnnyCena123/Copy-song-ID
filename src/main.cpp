4145be48e9b4af0d92fac225f3c5d37da4a57cf8
JohnnyCena123 <iamahuman1395@gmail.com>
2025-03-31 12:31:40 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,12 @@ class $modify(MyCustomSongWidget, CustomSongWidget) {
 		if (!m_downloadBtn || m_customSongID < 1 || m_isRobtopSong) return true;
 
 		CCSprite* copyBtnSpr = CCSprite::createWithSpriteFrameName("diffIcon_03_btn_001.png");
+                copyBtnSpr->setScale(0.7f);
 
 		CCMenuItemSpriteExtra* copyBtn = CCMenuItemSpriteExtra::create(copyBtnSpr, this, menu_selector(MyCustomSongWidget::onCopySongID));
 		copyBtn->setPosition({m_downloadBtn->getPositionX() - 31.f, m_downloadBtn->getPositionY() - 5.f});
 		if (isMusicLibrary) {
-			copyBtn->setPosition(ccp(m_playbackBtn->getPositionX() + 20.f, m_playbackBtn->getPositionY() + 22.5f));
+			copyBtn->setPosition(ccp(m_playbackBtn->getPositionX() + 60.f, m_playbackBtn->getPositionY() + 22.5f));
 		} else if (showSongSelect) copyBtn->setPosition({m_selectSongBtn->getPositionX() + 19.f, m_selectSongBtn->getPositionY() + 60.f});
 		copyBtn->setID("button"_spr);
 

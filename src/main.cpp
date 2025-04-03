240fb16b84294e2dcd57246976f4980f46c39ff9
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-04-02 22:19:03 -0400

    alright here goes nothing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 using namespace geode::prelude;
 
 class $modify(MyCustomSongWidget, CustomSongWidget) {
+	static void onModify(auto& self) {
+		(void) self.setHookPriorityAfterPost("CustomSongWidget::updateWithMultiAssets", "spaghettdev.songpreview");
+	}
+
 	bool init(SongInfoObject* songInfo, CustomSongDelegate* songDelegate, bool showSongSelect, bool showPlayMusic, bool showDownload, bool isRobtopSong, bool unkBool, bool isMusicLibrary, int unk) {
 		if (!CustomSongWidget::init(songInfo, songDelegate, showSongSelect, showPlayMusic, showDownload, isRobtopSong, unkBool, isMusicLibrary, unk)) return false;
 
@@ -13,13 +17,19 @@ class $modify(MyCustomSongWidget, CustomSongWidget) {
 		copySongIDButtonSprite->setScale(isMusicLibrary ? 0.375f : 0.5f);
 
 		CCMenuItemSpriteExtra* copySongIDButton = CCMenuItemSpriteExtra::create(copySongIDButtonSprite, this, menu_selector(MyCustomSongWidget::onCopySongID));
-		copySongIDButton->setPositionX(m_downloadBtn->getPositionX() + (isMusicLibrary ? 20.f : 25.f));
-		copySongIDButton->setPositionY(m_downloadBtn->getPositionY() + (isMusicLibrary ? 20.f : 60.f));
+		copySongIDButton->setPositionX(m_downloadBtn->getPositionX() + (isMusicLibrary ? 20.f : 21.5f));
+		copySongIDButton->setPositionY(m_downloadBtn->getPositionY() + (isMusicLibrary ? 20.f : 61.5f));
 		copySongIDButton->setID("copy-song-id-button"_spr);
 
 		m_buttonMenu->addChild(copySongIDButton);
 
 		return true;
+	}
+
+	void updateWithMultiAssets(gd::string p0, gd::string p1, int p2) {
+		CustomSongWidget::updateWithMultiAssets(p0, p1, p2);
+		if (!m_infoBtn || m_songs.empty() && m_sfx.empty() || m_playbackBtn->isVisible() || m_isMusicLibrary) return;
+		m_infoBtn->setPositionX(m_infoBtn->getPositionX() - 5.f);
 	}
 
 	void onCopySongID(CCObject* sender) {
